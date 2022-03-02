b7fda1cad08df4304a090d7c64d94745a04e6bfa
Andrew Sutton <97402701+errorless-stone@users.noreply.github.com>
2022-03-01 23:20:41 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,53 @@
-$(document).ready(function () {
-  console.log("doc is ready");
- 
-  let revName; 
+$(function () {
+  console.log("ready!");
 
-
-  $
-    ("#btnGetFacts").click(function (){
-
-      // handle it if input is blank
-
-      let userName = $("#userName").val();
-      let coolFacts = generateCoolNameFacts(userName); 
-
-      console.log('you clicked the button');
-      console.log('from ln 12', generateCoolNameFacts(userName));
-      
-      $("#coolNameFactsOutput").html(coolFacts);
-
-      $('#btnGetFacts').html(
-        '<button id="btnGetFacts" class="btn btn-secondary">click to play again</button>'
-      )
-
-      $('#btnGetFacts').click(function(){
-        location.reload();
+  // loads data into DevSkills card
+  $.each(skillData, function (key, item) {
+    $("#stuff-lists").append(`<h4> ${key} </h4> <dl id="${key}-list">`);
+    $.each(item, function (index, val) {
+      // $(`#${key}-list`).append(val.propertyName);
+        $.each(val, function (propertyName, propertyValue) {
+        console.log(propertyName, propertyValue);
+        //$(`#${key}-list`).append(`<li>`);
+          $(`#${key}-list`).append(`${propertyValue}`);
+        //${val.showName} / ${val.venu/e}`);
       });
-  
-
-
     });
+  });
 
+  // loads data into interest/hobbies card
+  $.each(interestData, function (key, item) {
+    $("#stuff-lists").append(`<h4> ${key} </h4> <dl id="${key}-list">`);
+    $.each(item, function (index, val) {
+      // $(`#${key}-list`).append(val.propertyName);
+        $.each(val, function (propertyName, propertyValue) {
+        console.log(propertyName, propertyValue);
+        //$(`#${key}-list`).append(`<li>`);
+          $(`#${key}-list`).append(`${propertyValue}`);
+        //${val.showName} / ${val.venu/e}`);
+      });
+    });
+  });
 
 });
 
-function generateCoolNameFacts(userName) {
-  let coolFacts = ''; 
-  
-  coolFacts = nameLength(userName);
-  coolFacts += " <br> " + nameStart(userName);
-  coolFacts += "<br>" + nameEnd(userName);
-  coolFacts += "<br>" + "Your Lucky number is " + userLuckyNum;
 
-  console.log("initialized the'm coolFacts variable: " + coolFacts); 
+const skillData = {
+  skills: [
+    { skillName: `<dt>Python</dt>`, description: `<dd>2 intro classes</dd>` },
+    { skillName: `<dt>HTML`, description: `<dd>~1.5 years experience, few webpages<dd>` },
+    { skillName: `<dt>jQuery`, description: `<dd>< than a semester of class` },
+    { skillName: `<dt>CSS`, description:`<dd> < than a semester`},
+  ],
+};
 
-  return coolFacts;
-}
-
-function nameLength(name) {
-  return "Your name is " + name.length + " letters long!"; 
-}
-
-function nameStart(name) {
-  return "the first letter of your name is " + name[0]
-}
-
-function nameEnd(name) {
-  
-  let lastIndex = name.length - 1
-
-  return "the last letter of your name is " + name[lastIndex]
-}
-
-var userLuckyNum = 1 + Math.floor(Math.random() * 100);
+const interestData = {
+  interests: [
+    { interestName: `<dt>Music (artists)</dt>`, description: `<dd>Scarface, Bob Dylan, Queen, Freddie Dredd, Michael Jackson, CCR, Queens of the Stone Age</dd>` },
+    { interestName: `<dt>Videogames (Titles/Series)`, description: `<dd>Most any FromSoft game, Elder Scrolls, Fallout, TF2, Garry's Mod, Pokemon, Payday 2, Halo, Metroid,
+     Hobo: Tough Life, Besiege, CoD (not many after Black Ops), Doom (Old & New), Tons of Indie titles (FTL, Insryption, Pixel Piracy, etc.)<dd>` },
+    { interestName: `<dt>Shows/YT Series`, description: `<dd>Dead Meat's "Kill Counts", Jacob Geller and Ryan Hollinger's video essays (Horror Dissections), Netflix's "Don't f***k with cats" 
+    & "Love, Death and Robots", Qxir's "Tales from the Bottle"` },
+    { interestName: `<dt>Comics/Manga`, description:`<dd> AVP, Berserk, anything Junji Ito`},
+  ],
+};
